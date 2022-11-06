700da3eb1af054de8ef7dabc7bff3c6e59091785
maxtli <maxtli068@gmail.com>
2022-11-06 02:11:38 -0500

    i pood

---
+++ poo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void sayPoo() {
+    for (int i = 0; i < 10000; ++i) {
+        cout << "poo" << endl;
+        cout << "shit" << endl;
+        cout << "cum" << endl;
+    }
+}
+
+int main() {
+    sayPoo();
+}
+
